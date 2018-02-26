65d7499f2c5fe6a90b848ba9bedfcb329d90056e
Ihar Padbiarezski <yb.igrok.by@gmail.com>
2018-02-26 16:46:25 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function solveEquation(equation) {
   // your implementation
+  let coef = equation.replace(/ /g, '').match(/[^\^]\d+/g);
+  let D = Math.pow(coef[1], 2) - 4 * coef[0] * coef[2];
+
+  return [
+    (-coef[1] - Math.round(Math.sqrt(D))) / (2 * coef[0]),
+    (-coef[1] + Math.round(Math.sqrt(D))) / (2 * coef[0])
+  ].sort((a, b) => a - b);
 }
